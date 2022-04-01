b0ece0dde4c2799dc42b05b5f4060701cb92e855
sanika <sanika.patwardhan@gmail.com>
2022-04-01 16:27:11 -0400

    first commit

---
+++ To do list/javascript.js
@@ -0,0 +1,27 @@
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+  }
+ document.getElementById("myUL").addEventListener("click",function(e) {
+    var tgt = e.target;
+    if (tgt.tagName.toUpperCase() == "LI") {
+        tgt.parentNode.removeChild(tgt);
+    }
+});
+
+
+/*var list = document.querySelector("ul").children;
+list.addEventListener('click', function(ev) 
+document.querySelector("ul").removechild
+)
+
+/*var item = document.getElementById(myUL.value);
+/*            a.removeChild(item);
+/*)})*/
